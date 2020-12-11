5c6f4b7a0f9b615f0462a60fbb46f74c5db922d5
Management <manav.rathod@berkeley.edu>
2020-12-10 19:03:09 -0800

    updated TRPOTutor

--- deeptutor/tutors/TRPOTutor.py
+++ deeptutor/tutors/TRPOTutor.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import torch
 
 from deeptutor.envs.MyGymEnv import MyGymEnv
 from deeptutor.policies.LoggedTRPO import LoggedTRPO
@@ -8,11 +8,12 @@ from garage import wrap_experiment
 from garage.experiment.deterministic import set_seed
 from garage.np.baselines import LinearFeatureBaseline
 from garage.sampler import FragmentWorker, LocalSampler
-from garage.tf.policies import CategoricalMLPPolicy
-from garage.torch.policies import CategoricalGRUPolicy
+from garage.tf.policies import CategoricalGRUPolicy
+# from garage.torch.policies import CategoricalGRUPolicy
 from garage.torch.q_functions import DiscreteMLPQFunction
 from garage.torch.value_functions import GaussianMLPValueFunction
-from garage.trainer import TFTrainer
+from garage.trainer import Trainer, TFTrainer
+from garage.torch import prefer_gpu, global_device
 
 
 class TRPOTutor(RLTutor):
@@ -20,21 +21,19 @@ class TRPOTutor(RLTutor):
         super().__init__(n_items)
     
     def train(self, gym_env, n_eps=10, seed=0):
-        tf.compat.v1.reset_default_graph()
         @wrap_experiment(archive_launch_repo=False, snapshot_mode="none")
         def train_trpo(ctxt=None):
             set_seed(seed)
-            with TFTrainer(snapshot_config=ctxt) as trainer:
+            with TFTrainer(ctxt) as trainer:
                 env = MyGymEnv(gym_env, max_episode_length=100)
-                policy = CategoricalMLPPolicy(name='policy',
-                                      env_spec=env.spec,
-                                      )
+                policy = CategoricalGRUPolicy(name='policy', env_spec=env.spec)
                 baseline = LinearFeatureBaseline(env_spec=env.spec)
                 sampler = LocalSampler(
                     agents=policy,
                     envs=env,
                     max_episode_length=env.spec.max_episode_length,
                     worker_class=FragmentWorker,
+                    is_tf_worker=True,
                 )
                 self.algo = LoggedTRPO(
                     env=env,
@@ -43,10 +42,11 @@ class TRPOTutor(RLTutor):
                     baseline=baseline,
                     sampler=sampler,
                     discount=0.99,
-                    max_kl_step=0.01,
+                    center_adv=False
                 )
 
                 trainer.setup(self.algo, env)
                 trainer.train(n_epochs=n_eps, batch_size=4000)
+                return self.algo.rew_chkpts
 
         return train_trpo()
